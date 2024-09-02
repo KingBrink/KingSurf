f019a19e37fa8182fdd460e904ffe2dd2dc2a47e
KingBrink <kyle.brink1602@gmail.com>
2024-09-02 09:20:30 +0200

    users

--- Model/db.js
+++ Model/db.js
@@ -1,269 +1,342 @@
-import { pool } from '../config/config.js'
+import { pool } from "../config/config.js";
 
-
-const getProducts = async() => {
-    const [products] = await pool.query(`
+const getProducts = async () => {
+  const [products] = await pool.query(`
      SELECT * FROM products
     `);
-    return products;
-}
+  return products;
+};
 
 // returns product by an id
 const getProductByID = async (id) => {
-    const [product] = await pool.query(`
+  const [product] = await pool.query(
+    `
         SELECT * FROM products WHERE product_id = ?
-    `, [id]);
+    `,
+    [id]
+  );
 
-    if(!id || isNaN(id) || id > product){
+  if (!id || isNaN(id) || id > product) {
+    throw error();
+  }
 
-        throw error()
+  return product;
+};
 
-    }
-
-    return product;
-}
-
-
-const editProduct = async (product_name, product_desc, product_price, prooduct_img, product_category, product_id) => {
-    const [product] = await pool.query(`
+const editProduct = async (
+  product_name,
+  product_desc,
+  product_price,
+  prooduct_img,
+  product_category,
+  product_id
+) => {
+  const [product] = await pool.query(
+    `
         UPDATE products SET product_name = ?, product_desc = ?, product_price = ?, product_img = ?, product_category = ? WHERE product_id = ?
-    `, [product_name, product_desc, product_price, prooduct_img, product_category, product_id]);
-}
+    `,
+    [
+      product_name,
+      product_desc,
+      product_price,
+      prooduct_img,
+      product_category,
+      product_id,
+    ]
+  );
+};
 
-
-const deleteProduct = async(id) => {
-    const [product] = await pool.query(`
+const deleteProduct = async (id) => {
+  const [product] = await pool.query(
+    `
     DELETE FROM products WHERE product_id =?
-    `, [id]);
-    if(!id || isNaN(id) || id > product){
+    `,
+    [id]
+  );
+  if (!id || isNaN(id) || id > product) {
+    throw error();
+  }
+};
 
-        throw error()
-
-    }
-}
-
-const addProduct = async (product_name, product_desc, product_price, product_img, product_category) => {
-    const [product] = await pool.query(`
+const addProduct = async (
+  product_name,
+  product_desc,
+  product_price,
+  product_img,
+  product_category
+) => {
+  const [product] = await pool.query(
+    `
     INSERT INTO products (product_name, product_desc, product_price, product_img, product_category) VALUES (?, ?, ?, ?, ?)
-    `,[product_name, product_desc, product_price, product_img, product_category]);
-    return getProducts(product.insertId)
-}
-
-
-
+    `,
+    [product_name, product_desc, product_price, product_img, product_category]
+  );
+  return getProducts(product.insertId);
+};
 
 // Cart settings ===============
-const getCart = async () => {
-    const [cart] = await pool.query(`
-        SELECT * FROM cart
-    `)
-    return cart
-}
-
-const addedInCart = async (user_id) => {
-    const [cartItems] = await pool.query(`
-        SELECT 
+const getCart = async (user_id) => {
+  const [cart] = await pool.query(
+    `
+        SELECT
+            cart.cartID,
             cart.quantity,
+            products.product_name,
             products.product_price,
             (cart.quantity * products.product_price) AS total_price,
-            products.product_img AS prodUrl,
-            products.product_name AS prodName,
-            products.product_id AS prodID
-        FROM 
+            products.product_img
+        FROM
             cart
-        JOIN 
+        JOIN
             products ON cart.product_id = products.product_id
-        WHERE 
+        WHERE
             cart.user_id = ?
-    `, [user_id]);
-    
-    return cartItems;
-}
-
+    `,
+    [user_id]
+  );
+  return cart;
+};
+const addedInCart = async (user_id) => {
+  const [cartItems] = await pool.query(
+    `
+        SELECT
+            cart.quantity,
+            products.product_price,
+            (cart.quantity * products.product_price) AS totalPrice,
+            products.product_img AS product_img,
+            products.product_name AS product_name,
+            products.product_id AS product_id
+        FROM
+            cart
+        JOIN
+            products ON cart.product_id = products.product_id
+        WHERE
+            cart.user_id = ?
+    `,
+    [user_id]
+  );
+  return cartItems;
+};
 const addToCart = async (product_id, user_id) => {
-    // Check if the product is already in the cart
-    const [existingProduct] = await pool.query(`
+  const [existingProduct] = await pool.query(
+    `
         SELECT * FROM cart
         WHERE product_id = ? AND user_id = ?
-    `, [product_id, user_id]);
-
-    if (existingProduct.length > 0) {
-
-        // Update the quantity of the existing product in the cart
-        const updatedQuantity = existingProduct[0].quantity + 1;
-
-        await pool.query(`
+    `,
+    [product_id, user_id]
+  );
+  if (existingProduct.length > 0) {
+    const updatedQuantity = existingProduct[0].quantity + 1;
+    await pool.query(
+      `
             UPDATE cart
             SET quantity = ?
             WHERE product_id = ? AND user_id = ?
-        `, [updatedQuantity, product_id, user_id]);
-
-    } else {
-
-        // Insert the product into the cart
-        await pool.query(`
+        `,
+      [updatedQuantity, product_id, user_id]
+    );
+  } else {
+    await pool.query(
+      `
             INSERT INTO cart (product_id, user_id, quantity)
             VALUES (?, ?, 1)
-        `, [product_id, user_id]);
-
-    }
-}
-
-const insert = async(product_id, user_id) => {
-    // Call the addToCart function to add the product to the cart
-    await addToCart(product_id, user_id);
-}
-
+        `,
+      [product_id, user_id]
+    );
+  }
+};
+const insert = async (product_id, user_id) => {
+  await addToCart(product_id, user_id);
+};
+const editCart = async (product_id, user_id, quantity, cartID) => {
+  if (!cartID || isNaN(cartID)) {
+    throw new Error("Invalid cart ID");
+  }
+  const [result] = await pool.query(
+    `
+        UPDATE cart
+        SET product_id = ?, quantity = ?
+        WHERE cartID = ? AND user_id = ?
+    `,
+    [product_id, quantity, cartID, user_id]
+  );
+  if (result.affectedRows === 0) {
+    throw new Error(
+      "Update failed, possibly due to an invalid cart ID or no changes made"
+    );
+  }
+  return result;
+};
 const removeFromCart = async (product_id, user_id) => {
-    // Check if the product is already in the cart
-    const [existingProduct] = await pool.query(`
+  const [existingProduct] = await pool.query(
+    `
         SELECT * FROM cart
         WHERE product_id = ? AND user_id = ?
-    `, [product_id, user_id]);
-
-    if (existingProduct.length > 0) {
-        // Decrease the quantity of the existing product in the cart
-        const updatedQuantity = existingProduct[0].quantity - 1;
-
-        if (updatedQuantity <= 0) {
-            // If the updated quantity is less than or equal to zero, remove the product from the cart
-            await pool.query(`
+    `,
+    [product_id, user_id]
+  );
+  if (existingProduct.length > 0) {
+    const updatedQuantity = existingProduct[0].quantity - 1;
+    if (updatedQuantity <= 0) {
+      await pool.query(
+        `
                 DELETE FROM cart
                 WHERE product_id = ? AND user_id = ?
-            `, [product_id, user_id]);
-        } else {
-            // Update the quantity of the existing product in the cart
-            await pool.query(`
+            `,
+        [product_id, user_id]
+      );
+    } else {
+      await pool.query(
+        `
                 UPDATE cart
                 SET quantity = ?
                 WHERE product_id = ? AND user_id = ?
-            `, [updatedQuantity, product_id, user_id]);
-        }
+            `,
+        [updatedQuantity, product_id, user_id]
+      );
     }
-}
-
-
+  }
+};
+// New function to delete a specific item
+const deleteSpecificItem = async (itemID, user_id) => {
+  await pool.query(
+    `
+        DELETE FROM cart
+        WHERE cartID = ? AND user_id = ?
+    `,
+    [itemID, user_id]
+  );
+};
 
 // user verification ============
 
 const checkUser = async (user_email, user_role) => {
-    const [[{user_password}]] = await pool.query(`
+  const [[{ user_password }]] = await pool.query(
+    `
         SELECT user_password FROM users WHERE user_email = ? AND user_role = ?
-    `, [user_email, user_role])
-    return user_password
-}
+    `,
+    [user_email, user_role]
+  );
+  return user_password;
+};
 
 // user settings
 
 const getUsers = async () => {
-    const [users] = await pool.query(`
+  const [users] = await pool.query(`
         SELECT * FROM users
     `);
-    return users;
-}
+  return users;
+};
 
-const addUser = async (user_profile, user_email, user_password, user_role, user_image) => {
-    const [user] = await pool.query(`
+const addUser = async (
+  user_profile,
+  user_email,
+  user_password,
+  user_role,
+  user_image
+) => {
+  const [user] = await pool.query(
+    `
        INSERT INTO users (user_profile, user_email, user_password, user_role, user_image) 
        VALUES(?, ?, ?, ?, ?)
-    `,[user_profile, user_email, user_password, user_role, user_image])
-    return getUsers(user.insertId)
-}
+    `,
+    [user_profile, user_email, user_password, user_role, user_image]
+  );
+  return getUsers(user.insertId);
+};
 
-const getUserByID =  async (user_id) => {
-    const [user] = await pool.query(`
+const getUserByID = async (user_id) => {
+  const [user] = await pool.query(
+    `
         SELECT * FROM users WHERE user_id = ?
-    `,[user_id]);
+    `,
+    [user_id]
+  );
 
-    if(!user_id || isNaN(user_id) || user_id > user){
+  if (!user_id || isNaN(user_id) || user_id > user) {
+    throw error();
+  }
 
-        throw error()
+  return user;
+};
 
-    }
-
-    return user
-}
-
-
-const editUser = async (user_profile, user_email, user_password, user_role, user_image, user_id) =>{
-    const [users] = await pool.query(`
+const editUser = async (
+  user_profile,
+  user_email,
+  user_password,
+  user_role,
+  user_image,
+  user_id
+) => {
+  const [users] = await pool.query(
+    `
     UPDATE users SET user_profile = ?, user_email = ?, user_password = ?, user_role = ?, user_image = ? WHERE user_id = ?
-    `, [user_profile, user_email, user_password, user_role, user_image, user_id]);
+    `,
+    [user_profile, user_email, user_password, user_role, user_image, user_id]
+  );
 
-    if(!user_id || isNaN(user_id) || user_id > users){
+  if (!user_id || isNaN(user_id) || user_id > users) {
+    throw error();
+  }
+};
 
-        throw error()
-
-    }
-}
-
-   
 const deleteUser = async (user_id) => {
-    const [users] = await pool.query(`
+  const [users] = await pool.query(
+    `
         DELETE FROM users WHERE user_id = ?
-    `,[user_id])
+    `,
+    [user_id]
+  );
 
-    if(!user_id || isNaN(user_id) || user_id > users){
-
-        throw error()
-
-    }
-}
+  if (!user_id || isNaN(user_id) || user_id > users) {
+    throw error();
+  }
+};
 
 // admin check
 const checkRoleStatus = async (user) => {
- const [[{user_role}]] = await pool.query(`
+  const [[{ user_role }]] = await pool.query(
+    `
     SELECT * FROM users WHERE user_role = ?
- ` , [user]);
-    return user_role;
-}
+ `,
+    [user]
+  );
+  return user_role;
+};
 
 const checkProfile = async (user_email) => {
-    const [validUser] = await pool.query(`
+  const [validUser] = await pool.query(
+    `
     SELECT * FROM users WHERE user_email = ?
-    `, [user_email]);
-    return validUser
-}
-  
- 
-export {getProducts, getProductByID, editProduct, deleteProduct, addProduct, getCart, addedInCart, insert, addToCart, removeFromCart, checkUser, getUsers, addUser, deleteUser, getUserByID, checkRoleStatus, editUser, checkProfile}
+    `,
+    [user_email]
+  );
+  return validUser;
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export {
+  getProducts,
+  getProductByID,
+  editProduct,
+  deleteProduct,
+  addProduct,
+  getCart,
+  addedInCart,
+  insert,
+  addToCart,
+  removeFromCart,
+  checkUser,
+  getUsers,
+  addUser,
+  deleteUser,
+  getUserByID,
+  checkRoleStatus,
+  editUser,
+  checkProfile,
+  editCart,
+  deleteSpecificItem,
+};
 
 // const addToCart = async (product_id) => {
 //     const [addToCart] = await pool.query(`
