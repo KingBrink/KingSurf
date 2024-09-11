4e28c700eae312b82a3402ab55b7b5d9a3b1b851
KingBrink <kyle.brink1602@gmail.com>
2024-09-11 23:51:14 +0200

    final5

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 // Routes
 app.post('/login', authenticate, (req, res) => {
-    res.json({ message: 'Logged in successfully!' });
+    res.json({ message: 'Logged in Successfully!' });
 });
 app.delete('/logout', (req, res) => {
     res.clearCookie('jwt');
